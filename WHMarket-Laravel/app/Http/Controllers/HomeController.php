471918a253a1774143acb258937d3b1e8a5e4091
Wika Fajriah R <wikafajriah@gmail.com>
2021-06-01 11:08:09 +0700

    Pulling from master

--- WHMarket-Laravel/app/Http/Controllers/HomeController.php
+++ WHMarket-Laravel/app/Http/Controllers/HomeController.php
@@ -43,4 +43,8 @@ class HomeController extends Controller
     {
         return view('pemeriksaanbarang');
     }
+    public function datapegawai()
+    {
+        return view('datapegawai');
+    }
 }
