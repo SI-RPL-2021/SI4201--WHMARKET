f1e20110a16a8335e7268121062c423880192282
HadinaWawesi <wawesihadina@gmail.com>
2021-06-08 07:50:02 +0700

    Sprint 3 - Minggu 2 - update file data supplier controller ref #8 #46

--- WHMarket-Laravel/app/Http/Controllers/DataSupplierController.php
+++ WHMarket-Laravel/app/Http/Controllers/DataSupplierController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class DataSupplierController extends Controller
 {
-    //
+    public function tambah_datasupplier()
+    {
+        return view('tambah_datasupplier');
+    }
+    public function datasupplier()
+    {
+        $datasupplier = \DB::table('datasupplier')->get();
+        return view('datasupplier', ['datasupplier' => $datasupplier]);
+    }
+    public function delete($id)
+    {
+        \DB::table('datasupplier')->where('id', $id)->delete();
+        return redirect('datasupplier'); //
+    }
 }
