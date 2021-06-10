b651c24bba6585e54febb62b072a733dd5bf24cc
Jonathan Bonar <jonathanranob@gmail.com>
2021-06-10 09:21:36 +0700

    Sprint 3 - Minggu 2 - Mengupdate file data supplier controller ref #44 #45

--- WHMarket-Laravel/routes/web.php
+++ WHMarket-Laravel/routes/web.php
@@ -27,6 +27,7 @@ Route::get('data_barangmasuk', 'App\Http\Controllers\HomeController@data_barangm
 Route::get('data_barangkeluar', 'App\Http\Controllers\HomeController@data_barangkeluar');
 Route::get('pemeriksaanbarang', 'App\Http\Controllers\HomeController@pemeriksaanbarang');
 Route::get('datapegawai', 'App\Http\Controllers\HomeController@datapegawai');
+Route::get('datasupplier', 'App\Http\Controllers\HomeController@datasupplier');
 
 Route::get('tambah_masterbarang', 'App\Http\Controllers\MasterBarangController@tambah_masterbarang');
 Route::get('tambah_mastersatuan', 'App\Http\Controllers\MasterSatuanController@tambah_mastersatuan');
@@ -36,6 +37,7 @@ Route::get('tambah_data_stokbarang', 'App\Http\Controllers\DataStokBarangControl
 Route::get('tambah_data_barangmasuk', 'App\Http\Controllers\BarangMasukController@tambah_data_barangmasuk');
 Route::get('tambah_data_barangkeluar', 'App\Http\Controllers\BarangKeluarController@tambah_data_barangkeluar');
 Route::get('tambah_pemeriksaanbarang', 'App\Http\Controllers\PemeriksaanBarangController@tambah_pemeriksaanbarang');
+Route::get('tambah_datasupplier', 'App\Http\Controllers\DataSupplierController@tambah_datasupplier');
 
 Route::get('masterkemasan', 'App\Http\Controllers\MasterKemasanController@data_masterkemasan');
 Route::post('masterkemasan', 'App\Http\Controllers\MasterKemasanController@inputMasterKemasan');
@@ -89,4 +91,7 @@ Route::get('datapegawai', 'App\Http\Controllers\DataPegawaiController@datapegawa
 Route::delete('datapegawai/{id}', 'App\Http\Controllers\DataPegawaiController@delete');
 
 Route::get('datasupplier', 'App\Http\Controllers\DataSupplierController@datasupplier');
+Route::post('datasupplier', 'App\Http\Controllers\DataSupplierController@InputDataSupplier');
+Route::get('edit_datasupplier/{id}', 'App\Http\Controllers\DataSupplierController@updateDataSupplier');
+Route::patch('datasupplier/{id}', 'App\Http\Controllers\DataSupplierController@updateDataSupplierProcess');
 Route::delete('datasupplier/{id}', 'App\Http\Controllers\DataSupplierController@delete');
