dbff7da9e70ce1a6c0a541ba27f9aad1307245c7
Wika Fajriah R <wikafajriah@gmail.com>
2021-06-10 09:13:02 +0700

    Sprint 3 - Minggu 2 - Mengupdate file data pegawai ref #49 #48

--- WHMarket-Laravel/app/Http/Controllers/DataPegawaiController.php
+++ WHMarket-Laravel/app/Http/Controllers/DataPegawaiController.php
@@ -15,6 +15,38 @@ class DataPegawaiController extends Controller
         $datapegawai = \DB::table('datapegawai')->get();
         return view('datapegawai', ['datapegawai' => $datapegawai]);
     }
+    public function inputDataPegawai(Request $request)
+    {
+        
+        \DB::table('datapegawai')->insert([
+            'nama_pegawai' => $request->nama_pegawai,
+            'user_id' => $request->user_id,
+            'alamat' => $request->alamat,
+            'kota' => $request->kota,
+            'email' => $request->email,
+            'no_hp' => $request->no_hp
+        ]);
+
+            return redirect('datapegawai');
+    }
+    public function updateDataPegawai($id)
+    {
+        $datapegawai = \DB::table('datapegawai')->where('id', $id)->first();
+        return view('edit_datapegawai', compact('datapegawai'));
+    }
+    public function updateDataPegawaiProcess(Request $request, $id)
+    {
+        \DB::table('datapegawai')->where('id', $id)
+        ->update([
+            'nama_pegawai' => $request->nama_pegawai,
+            'user_id' => $request->user_id,
+            'alamat' => $request->alamat,
+            'kota' => $request->kota,
+            'email' => $request->email,
+            'no_hp' => $request->no_hp
+        ]);
+        return redirect('datapegawai');
+    }
     public function delete($id)
     {
         \DB::table('datapegawai')->where('id', $id)->delete();
