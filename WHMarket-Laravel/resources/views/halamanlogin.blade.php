a78f71c75d42deb2699d8f03fa6a66eba6f08c8c
Nabilla Prilya <nprilya.n@gmail.com>
2021-06-11 21:03:50 +0700

    Sprint 3 - Minggu 1 - Update revisi halaman login

--- WHMarket-Laravel/resources/views/halamanlogin.blade.php
+++ WHMarket-Laravel/resources/views/halamanlogin.blade.php
@@ -22,52 +22,44 @@
 </head>
 <body>
 
-<div class="container-fluid px-1 px-md-4 px-lg-1 px-xl-4 py-4 mx-auto">
+<div class="container-fluid px-1 px-md-5 px-lg-1 px-xl-5 py-5 mx-auto">
     <div class="card card0 border-0">
         <div class="row d-flex">
             <div class="col-lg-6">
                 <div class="card1 pb-5">
-                    <div class="row"> <img src="/assets/img/WHMARKET.png" width="50" height="40" class="logo"> </div>
-                    <div class="row px-3 justify-content-center mt-4 mb-5 border-line"> <img src="/assets/img/login-image.png" width="600" height="380">
-                     </div>
+                    <div class="row"> <img src="/assets/img/WHMARKET.png" width="30" height="40" class="logo"> </div>
+                    <div class="row px-3 justify-content-center mt-4 mb-5 border-line"> <img src="/assets/img/login-image.png" width="600" height="380"> </div>
                 </div>
             </div>
-            <div class="col-lg-6" style="padding-top: 2cm;">
-            <h3> Welcome Back. Lets Get Back To Work</h3>
+            <div class="col-lg-6">
                 <div class="card2 card border-0 px-4 py-5">
-                    <div class="row px-2"> <label class="mb-1">
-                            <h4>Email Address</h4>
-                        </label> <input class="mb-4" type="text" name="email" placeholder="Enter a valid email address"> </div>
-                    <div class="row px-2"> <label class="mb-1">
-                            <h4>Password</h4>
-                        </label> <input type="password" name="password" placeholder="Enter password"> </div>
-                        <br>
-                    <div class="row px-3 mb-4">
-                        <div class="custom-control custom-checkbox custom-control-inline"> <input id="chk1" type="checkbox" name="chk" class="custom-control-input">
+                    <div class="row mb-4 px-3">
+                        
                     </div>
-                    <div class="row mb-2 px-2"> <button type="submit" class="btn btn-blue text-center" style="width: 150px; height:40px;">Login</button> </div>
+                    <div class="row px-3 mb-4">
+                    <h3> Welcome Back. Lets Get Back To Work</h3>
+                    </div>
+                    <div class="row px-3"> <label class="mb-1">
+                    <br>
+                            <h6 class="mb-0 text-sm">Email Address</h6>
+                            <br>
+                        </label> <input class="mb-4" type="text" name="email" placeholder="Enter a valid email address"> </div>
+                    <div class="row px-3"> <label class="mb-1">
+            
+                            <h6 class="mb-0 text-sm">Password</h6>
+                            <br>
+                        </label> <input type="password" name="password" placeholder="Enter password"> </div>
+                    <div class="row px-3 mb-4">
+                    </div>
+                    <div class="row mb-3 px-3"> <button type="submit" class="btn btn-blue text-center">Login</button> </div>
                 </div>
             </div>
         </div>
         <div class="bg-blue py-4">
-            <div class="row px-3"> <small class="ml-4 ml-sm-5 mb-2">Copyright &copy; 2021. All rights reserved.</small>
+            <div class="row px-3"> <small class="ml-4 ml-sm-5 mb-2">Copyright &copy; 2021. WHMARKET All rights reserved.</small>
             </div>
         </div>
     </div>
-</div>
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"></script>
-
-    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-    -->
-    
+</div>    
  </body>
  <html>
