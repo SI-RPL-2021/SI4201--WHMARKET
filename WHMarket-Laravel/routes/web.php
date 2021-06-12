84f177b50232ae02a431f35f12ac9ab7907b58fe
Muhammad Syukron Kamil <msyukronkamil20@gmail.com>
2021-06-12 14:30:11 +0700

    Sprint 3 - Minggu 2 - Update file routes

--- WHMarket-Laravel/routes/web.php
+++ WHMarket-Laravel/routes/web.php
@@ -13,9 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 //
+
+
 Route::get('halamanlogin', 'App\Http\Controllers\AuthController@login')->name('halamanlogin');
 Route::post('InputLogin', 'App\Http\Controllers\AuthController@InputLogin');
-Route::get('logout', 'App\Http\Controllers\AuthController@logout');
 
 Route::group(['middleware' => 'auth'],function(){
 Route::get('home', 'App\Http\Controllers\HomeController@home');
@@ -28,6 +29,7 @@ Route::get('data_barangmasuk', 'App\Http\Controllers\HomeController@data_barangm
 Route::get('data_barangkeluar', 'App\Http\Controllers\HomeController@data_barangkeluar');
 Route::get('pemeriksaanbarang', 'App\Http\Controllers\HomeController@pemeriksaanbarang');
 Route::get('datapegawai', 'App\Http\Controllers\HomeController@datapegawai');
+Route::get('datasupplier', 'App\Http\Controllers\HomeController@datasupplier');
 
 Route::get('tambah_masterbarang', 'App\Http\Controllers\MasterBarangController@tambah_masterbarang');
 Route::get('tambah_mastersatuan', 'App\Http\Controllers\MasterSatuanController@tambah_mastersatuan');
@@ -37,6 +39,8 @@ Route::get('tambah_data_stokbarang', 'App\Http\Controllers\DataStokBarangControl
 Route::get('tambah_data_barangmasuk', 'App\Http\Controllers\BarangMasukController@tambah_data_barangmasuk');
 Route::get('tambah_data_barangkeluar', 'App\Http\Controllers\BarangKeluarController@tambah_data_barangkeluar');
 Route::get('tambah_pemeriksaanbarang', 'App\Http\Controllers\PemeriksaanBarangController@tambah_pemeriksaanbarang');
+Route::get('tambah_datasupplier', 'App\Http\Controllers\DataSupplierController@tambah_datasupplier');
+Route::get('tambah_datapegawai', 'App\Http\Controllers\DataSupplierController@tambah_datapegawai');
 
 Route::get('masterkemasan', 'App\Http\Controllers\MasterKemasanController@data_masterkemasan');
 Route::post('masterkemasan', 'App\Http\Controllers\MasterKemasanController@inputMasterKemasan');
@@ -87,8 +91,14 @@ Route::patch('pemeriksaanbarang/{id}', 'App\Http\Controllers\PemeriksaanBarangCo
 Route::delete('pemeriksaanbarang/{id}', 'App\Http\Controllers\PemeriksaanBarangController@delete');
 
 Route::get('datapegawai', 'App\Http\Controllers\DataPegawaiController@datapegawai');
+Route::post('datapegawai', 'App\Http\Controllers\DataPegawaiController@InputDataPegawai');
+Route::get('edit_datapegawai/{id}', 'App\Http\Controllers\DataPegawaiController@updateDataPegawai');
+Route::patch('datapegawai/{id}', 'App\Http\Controllers\DataPegawaiController@updateDataPegawaiProcess');
 Route::delete('datapegawai/{id}', 'App\Http\Controllers\DataPegawaiController@delete');
 
 Route::get('datasupplier', 'App\Http\Controllers\DataSupplierController@datasupplier');
+Route::post('datasupplier', 'App\Http\Controllers\DataSupplierController@InputDataSupplier');
+Route::get('edit_datasupplier/{id}', 'App\Http\Controllers\DataSupplierController@updateDataSupplier');
+Route::patch('datasupplier/{id}', 'App\Http\Controllers\DataSupplierController@updateDataSupplierProcess');
 Route::delete('datasupplier/{id}', 'App\Http\Controllers\DataSupplierController@delete');
 });
