f87baec80e554ca97679d46f87d5f7c20eb74008
Jonathan Bonar <jonathanranob@gmail.com>
2021-06-17 21:56:11 +0700

    Sprint 3 - Minggu 3 - Hide pemeriksaan barang

--- WHMarket-Laravel/resources/views/edit_datapegawai.blade.php
+++ WHMarket-Laravel/resources/views/edit_datapegawai.blade.php
@@ -65,7 +65,10 @@
            
         </ul>
     
-        <li><a href="{{ url('pemeriksaanbarang') }}"><i class="fas fa-store"></i>&nbsp; Pemeriksaan Barang</a></li>
+        @if(auth()->user()->role == 'superadmin')
+                <li><a href="{{ url('pemeriksaanbarang') }}"><i class="fas fa-store"></i>&nbsp; Pemeriksaan Barang</a>
+                </li>
+                @endif
    <li><a href="{{ url('dashboard_reporting') }}"><i class="fas fa-chart-bar"></i>&nbsp; Dashboard Reporting</a></li>
    <li><a href="{{ url('datasupplier') }}"><i class="fas fa-warehouse"></i>&nbsp; Data Supplier</a></li>
    <li><a href="{{ url('datapegawai') }}"><i class="fas fa-user"></i>&nbsp; Data Pegawai</a></li>
